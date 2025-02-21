1aaeeeea3fdc0112c67b85bd85fc1d1423bb169d
C:\Marcel.exe <fujitsustar@hotmail.de>
2025-02-21 04:19:10 +0100

    Add WeatherData type definition to lib/definitions.ts

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -36,6 +36,50 @@ export type TweetTable = {
   showUntil: string;
 };
 
+export type WeatherData = {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: Array<{
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }>;
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level?: number;
+    grnd_level?: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+};
+
 export interface Settings {
   general: {
     time: number;
@@ -60,7 +104,7 @@ export interface Settings {
       width: string;
       height: string;
       qrcode: boolean;
-    }
+    },
   ];
 }
 
