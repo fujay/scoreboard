14ae5dbf33ec6408c29810c67c5a84c1162c5069
C:\Marcel.exe <fujitsustar@hotmail.de>
2025-02-22 23:20:16 +0100

    Remove unused convertOpenWeatherToAnimateWeather function from utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -51,33 +51,6 @@ export function convertKelvinToCelsios(tempKelvin: number): number {
   return Math.floor(tempCelsius);
 }
 
-/**
- * Converts the weather description from OpenWeather to AnimateWeather format.
- *
- * @param weather - The weather description from OpenWeather.
- * @returns The weather description in AnimateWeather format.
- */
-export function convertOpenWeatherToAnimateWeather(weather: string): string {
-  switch (weather) {
-    case "Thunderstorm":
-      return "lightning";
-    case "Drizzle":
-      return "rainy";
-    case "Rain":
-      return "rainy";
-    case "Snow":
-      return "";
-    case "Atmosphere":
-      return "";
-    case "Clear":
-      return "sunny";
-    case "Clouds":
-      return "cloudy";
-    default:
-      return "";
-  }
-}
-
 export const formatDateToLocal = (
   dateStr: string,
   locale: string = "en-US",
