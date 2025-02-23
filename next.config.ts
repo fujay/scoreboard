334ecf6e82d7c8bce3006f32a31154aeaabb383c
C:\Marcel.exe <fujitsustar@hotmail.de>
2025-02-23 06:12:14 +0100

    Add remote pattern for OpenWeatherMap images in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
+        hostname: "openweathermap.org",
+      },
+      {
         hostname: "pbs.twimg.com",
       },
     ],
