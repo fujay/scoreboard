6768040ad81009f26cab3f0fa2522ce984257a4c
Marcel Klamm <fujitsustar@hotmail.de>
2025-03-02 23:32:26 +0100

    Refactor RSS page to integrate pagination, search functionality, and improve feed parsing

--- app/dashboard/rss/page.tsx
+++ app/dashboard/rss/page.tsx
@@ -1,40 +1,50 @@
 import { lusitana } from "@/ui/fonts";
+import Pagination from "@/ui/pagination";
+import Search from "@/ui/search";
+import { InvoicesTableSkeleton } from "@/ui/skeletons";
 import { Metadata } from "next";
+import { Suspense } from "react";
 import Parser from "rss-parser";
+import { CreateRSS } from "@/ui/rss/buttons";
+import Table from "@/ui/rss/table";
 
 export const metadata: Metadata = {
   title: "RSS",
 };
 
-type CustomFeed = { foo: string };
-type CustomItem = { bar: number };
-
-const parser: Parser<CustomFeed, CustomItem> = new Parser({
-  customFields: {
-    feed: ["foo", "baz"],
-    //            ^ will error because `baz` is not a key of CustomFeed
-    item: ["bar"],
-  },
-});
+const parser = new Parser();
 
 export default async function Page() {
-  const feed = await parser.parseURL("https://www.reddit.com/.rss");
-  console.log(feed.title); // feed will have a `foo` property, type as a string
+  const feed = await parser.parseURL(
+    "https://www.frankfurt-university.de/de/aktuelles/rss.xml",
+  );
+  console.log(feed);
 
-  feed.items.forEach((item) => {
-    console.log(item.title + ":" + item.link); // item will have a `bar` property type as a number
-  });
+  // feed.items.forEach((item) => {
+  //   console.log(item.title + ":" + item.link);
+  // });
 
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>RSS</h1>
       </div>
-      {feed.items.map((item) => (
-        <div className="border" key={item.guid}>
-          {item.title}
-        </div>
-      ))}
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search RSS..." />
+        <CreateRSS />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
     </div>
+
+    // {feed.items.map((item) => (
+    //   <div className="border" key={item.guid}>
+    //     {item.title}
+    //   </div>
+    // ))}
   );
 }
