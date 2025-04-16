1b5bbe7e4d56434caed824f76726c41c2c0e3d80
C:\Marcel.exe <fujitsustar@hotmail.de>
2025-04-16 20:38:26 +0200

    refactor: adjust layout for Home component and simplify structure

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,15 @@ export default async function Home() {
   const settings = await readConfig();
 
   return (
-    <div className="flex h-screen flex-col overflow-hidden">
+    <div className="flex min-h-screen flex-col overflow-hidden">
       <Header settings={settings.general.date} />
-      <main className="flex-1">
-        <BillboardDisplay
-          active={settings.weather.active}
-          location={settings.weather.location}
-          qrcode={settings.weather.qrcode}
-          graphic={settings.weather.graphic}
-          interval={settings.general.time}
-        />
-      </main>
-      {/* <footer className="">Progress</footer> */}
+      <BillboardDisplay
+        active={settings.weather.active}
+        location={settings.weather.location}
+        qrcode={settings.weather.qrcode}
+        graphic={settings.weather.graphic}
+        interval={settings.general.time}
+      />
     </div>
   );
 }
