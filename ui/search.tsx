85fa0aefdc83eb9aab6ede4a3bf9fa53d0e67f59
C:\Marcel.exe <fujitsustar@hotmail.de>
2025-04-17 05:00:33 +0200

    refactor: improve variable naming in Search component for clarity

--- ui/search.tsx
+++ ui/search.tsx
@@ -9,11 +9,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch = useDebouncedCallback((term) => {
+  const handleSearch = useDebouncedCallback((searchValue: string) => {
     const params = new URLSearchParams(searchParams);
     params.set("page", "1");
-    if (term) {
-      params.set("query", term);
+    if (searchValue) {
+      params.set("query", searchValue);
     } else {
       params.delete("query");
     }
@@ -33,7 +33,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         }}
         defaultValue={searchParams.get("query")?.toString()}
       />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+      <MagnifyingGlassIcon className="absolute top-1/2 left-3 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
 }
