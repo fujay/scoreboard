f33372b89e97553de67703a5cff256531f5655c8
C:\Marcel.exe <fujitsustar@hotmail.de>
2025-05-12 18:29:02 +0200

    feat: add CreateNews, UpdateNews, and DeleteNews components for news management

---
+++ ui/news/buttons.tsx
@@ -0,0 +1,42 @@
+import { deleteNews } from "@/lib/actions";
+import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+export function CreateNews() {
+  return (
+    <Link
+      href="/dashboard/news/create"
+      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+    >
+      <span className="hidden md:block">Create News</span>{" "}
+      <PlusIcon className="h-5 md:ml-4" />
+    </Link>
+  );
+}
+
+export function UpdateNews({ id }: { id: string }) {
+  return (
+    <Link
+      href={`/dashboard/news/${id}/edit`}
+      className="rounded-md border p-2 hover:bg-gray-100"
+    >
+      <PencilIcon className="w-5" />
+    </Link>
+  );
+}
+
+export function DeleteNews({ id }: { id: string }) {
+  const deleteNewsWithId = deleteNews.bind(null, id);
+
+  return (
+    <form action={deleteNewsWithId}>
+      <button
+        type="submit"
+        className="cursor-pointer rounded-md border p-2 hover:bg-gray-100 hover:text-red-500"
+      >
+        <span className="sr-only">Delete</span>
+        <TrashIcon className="w-5" />
+      </button>
+    </form>
+  );
+}
