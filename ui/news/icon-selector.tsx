b5428304c19b413765260469cb139508c0c1767c
C:\Marcel.exe <fujitsustar@hotmail.de>
2025-05-13 20:56:11 +0200

    feat: enhance IconSelector component to accept custom className for styling and tabbing

--- ui/news/icon-selector.tsx
+++ ui/news/icon-selector.tsx
@@ -5,19 +5,24 @@ import { NewsIconsMapping } from "@/lib/definitions";
 type IconSelectorProps = {
   selectedIcon: string | null;
   onSelectedIcon: (iconName: string) => void;
+  className?: string;
 };
 
 export default function IconSelector({
   selectedIcon,
   onSelectedIcon,
+  className,
 }: IconSelectorProps) {
   return (
-    <div className="flex flex-wrap justify-between rounded-md border p-2 md:gap-2">
+    <div
+      className={`flex flex-wrap justify-between rounded-md border p-2 md:gap-2 ${className}`}
+    >
       {NewsIconsMapping.map((icon) => (
         <div
           key={icon.name}
           className={`cursor-pointer rounded-md p-3 transition-colors hover:bg-primary/10 ${selectedIcon === icon.name ? "bg-primary/30 hover:bg-primary/30" : ""}`}
           onClick={() => onSelectedIcon(icon.name)}
+          tabIndex={0}
         >
           <icon.icon className="md:h-8 md:w-8" />
         </div>
