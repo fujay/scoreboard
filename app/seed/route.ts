1e82529f4aa33cfcc4a6ea553902d8a7acd82426
C:\Marcel.exe <fujitsustar@hotmail.de>
2025-05-13 04:19:02 +0200

    feat: add icon column with default value to news table

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -21,7 +21,8 @@ async function createNewsTable() {
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
       title TEXT NOT NULL,
       content TEXT NOT NULL,
-      display_until DATE
+      show_until DATE,
+      icon VARCHAR(100) NOT NULL DEFAULT 'info'
     );
   `;
 }
