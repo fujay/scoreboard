ba8c058fb358afce1e045e21b52562ac5db4677b
C:\Marcel.exe <fujitsustar@hotmail.de>
2025-06-11 14:38:15 +0200

    refactor: clean up imports and streamline social media display component

--- components/social-media-display.tsx
+++ components/social-media-display.tsx
@@ -1,11 +1,5 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { SocialMediaData } from "@/lib/definitions";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { QrCode } from "lucide-react";
 import { QRCodeSVG } from "qrcode.react";
 import {
@@ -17,6 +11,7 @@ import {
   XEmbed,
   YouTubeEmbed,
 } from "react-social-media-embed";
+import { Tweet } from "react-tweet";
 
 export default function SocialMediaDisplay({
   data,
@@ -25,7 +20,6 @@ export default function SocialMediaDisplay({
 }) {
   return (
     <main className="bg-gradient-to-b from-sky-100 to-sky-50 p-4 dark:from-slate-900 dark:to-slate-800">
-      {/* <div className="grid gap-4 md:grid-cols-2"> */}
       {/* Main Card */}
       <Card className="bg-white/50 backdrop-blur-sm dark:bg-slate-800/50">
         <CardHeader className="text-center">
@@ -35,15 +29,9 @@ export default function SocialMediaDisplay({
         </CardHeader>
         <CardContent className="flex justify-center text-4xl font-bold text-slate-900 dark:text-slate-100">
           {data.platform === "Facebook" ? (
-            <FacebookEmbed
-              // width={"100%"}
-              // height={"100%"}
-              url={data.url}
-              // className=""
-              // style={}
-            />
+            <FacebookEmbed url={data.url} />
           ) : data.platform === "Instagram" ? (
-            <InstagramEmbed url={data.url} />
+            <InstagramEmbed url={data.url} captioned />
           ) : data.platform === "LinkedIn" ? (
             <LinkedInEmbed url={data.url} />
           ) : data.platform === "Pinterest" ? (
@@ -52,28 +40,26 @@ export default function SocialMediaDisplay({
             <TikTokEmbed url={data.url} />
           ) : data.platform === "X" ? (
             <XEmbed url={data.url} />
+          ) : data.platform === "Tweet" ? (
+            <Tweet id={data.url} />
           ) : data.platform === "YouTube" ? (
-            <YouTubeEmbed url={data.url} />
+            <YouTubeEmbed
+              url={data.url}
+              youTubeProps={{
+                onReady(event) {
+                  event.target.playVideo();
+                },
+              }}
+            />
           ) : null}
-        </CardContent>
-
-        <CardFooter>
           {data.qrcode && (
-            <Card className="bg-white/50 backdrop-blur-sm dark:bg-slate-800/50">
-              <CardHeader>
-                <CardTitle className="flex items-center justify-center gap-2 text-2xl">
-                  <QrCode className="size-8 text-sky-500" />
-                  QR Code
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="flex items-center justify-center">
-                <QRCodeSVG value={data.url} />
-              </CardContent>
-            </Card>
+            <>
+              <QrCode className="size-8 text-sky-500" />
+              <QRCodeSVG value={data.url} />
+            </>
           )}
-        </CardFooter>
+        </CardContent>
       </Card>
-      {/* </div> */}
     </main>
   );
 }
