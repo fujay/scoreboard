1f7442abd29bb1982465015aaf9bfbbdc0a92a0e
C:\Marcel.exe <fujitsustar@hotmail.de>
2025-07-09 16:44:11 +0200

    feat: add skeleton components for data and categories charts

--- ui/skeletons.tsx
+++ ui/skeletons.tsx
@@ -32,6 +32,36 @@ export function CardsSkeleton() {
   );
 }
 
+export function DataChartSkeleton() {
+  return (
+    <div className={`${shimmer} relative w-full overflow-hidden md:col-span-4`}>
+      <div className="mb-4 h-8 w-36 rounded-md bg-gray-100" />
+      <div className="rounded-xl bg-gray-100 p-4">
+        <div className="mt-0 grid h-[410px] grid-cols-12 items-end gap-2 rounded-md bg-white p-4 sm:grid-cols-13 md:gap-4" />
+        <div className="flex items-center pt-6 pb-2">
+          <div className="h-5 w-5 rounded-full bg-gray-200" />
+          <div className="ml-2 h-4 w-20 rounded-md bg-gray-200" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function DataCategoriesChartSkeleton() {
+  return (
+    <div className={`${shimmer} relative w-full overflow-hidden md:col-span-4`}>
+      <div className="mb-4 h-8 w-36 rounded-md bg-gray-100" />
+      <div className="rounded-xl bg-gray-100 p-4">
+        <div className="mt-0 grid h-[410px] grid-cols-12 items-end gap-2 rounded-md bg-white p-4 sm:grid-cols-13 md:gap-4" />
+        <div className="flex items-center pt-6 pb-2">
+          <div className="h-5 w-5 rounded-full bg-gray-200" />
+          <div className="ml-2 h-4 w-20 rounded-md bg-gray-200" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function DashboardSkeleton() {
   return (
     <>
@@ -45,8 +75,8 @@ export default function DashboardSkeleton() {
         <CardSkeleton />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <DataChartSkeleton /> */}
-        {/* <DataCategoriesChartSkeleton /> */}
+        <DataChartSkeleton />
+        <DataCategoriesChartSkeleton />
       </div>
     </>
   );
