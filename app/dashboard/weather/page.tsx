437d72d1911ded6e7a30ede9c0172ad837679a77
C:\Marcel.exe <fujitsustar@hotmail.de>
2025-07-20 05:46:26 +0200

    fix: restore settings fetching from configuration and update Weather component usage

--- app/dashboard/weather/page.tsx
+++ app/dashboard/weather/page.tsx
@@ -1,6 +1,5 @@
 import Weather from "@/components/dashboard/weather";
-import { Settings } from "@/lib/definitions";
-// import { readKeyConfig } from "@/lib/config";
+import { readKeyConfig } from "@/lib/config";
 import { lusitana } from "@/ui/fonts";
 import { Metadata } from "next";
 
@@ -9,32 +8,14 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  // const settings = await readKeyConfig("weather");
-  const settings: Settings = {
-    general: {
-      time: 15,
-      db: "Remote",
-      images: "Remote",
-      stale: 10,
-      fetching: "SWR",
-      date: "Clock and Date without time",
-      news: "carousel",
-      progressbar: "ProgressBar and Countdown",
-    },
-    weather: {
-      active: true,
-      location: "Frankfurt am Main",
-      qrcode: true,
-      graphic: "Lucide Icons",
-    },
-  };
+  const settings = await readKeyConfig("weather");
 
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Weather</h1>
       </div>
-      <Weather settings={settings.weather} />
+      <Weather settings={settings} />
     </div>
   );
 }
