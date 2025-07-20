8202271b9c11ced42e3384d2382c6a461310850a
C:\Marcel.exe <fujitsustar@hotmail.de>
2025-07-20 04:49:02 +0200

    fix: restore readConfig usage in Home component and comment out static settings definition

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,28 @@
 import BillboardDisplay from "@/components/billboard-display";
 import Header from "@/components/header";
-import { Settings } from "@/lib/definitions";
-// import { readConfig } from "@/lib/config";
+// import { Settings } from "@/lib/definitions";
+import { readConfig } from "@/lib/config";
 
 export default async function Home() {
-  // const settings = await readConfig();
-  const settings: Settings = {
-    general: {
-      time: 15,
-      db: "Remote",
-      images: "Remote",
-      stale: 10,
-      fetching: "SWR",
-      date: "Clock and Date without time",
-      news: "carousel",
-      progressbar: "ProgressBar and Countdown",
-    },
-    weather: {
-      active: true,
-      location: "Frankfurt am Main",
-      qrcode: true,
-      graphic: "Lucide Icons",
-    },
-  };
+  const settings = await readConfig();
+  // const settings: Settings = {
+  //   general: {
+  //     time: 15,
+  //     db: "Remote",
+  //     images: "Remote",
+  //     stale: 10,
+  //     fetching: "SWR",
+  //     date: "Clock and Date without time",
+  //     news: "carousel",
+  //     progressbar: "ProgressBar and Countdown",
+  //   },
+  //   weather: {
+  //     active: true,
+  //     location: "Frankfurt am Main",
+  //     qrcode: true,
+  //     graphic: "Lucide Icons",
+  //   },
+  // };
 
   return (
     <div className="flex min-h-screen flex-col overflow-hidden">
